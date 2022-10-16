dde5ef186c1c1118b5e52797be90dc84b8437ecb
Stephan Michailenko <94607203+stephan-art@users.noreply.github.com>
2022-10-16 15:52:58 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	if (N == 12 || N == 1 || N == 2)
+		cout << "Winter";
+	if (N == 3 || N == 4 || N == 5)
+		cout << "Spring";
+	if (N == 6 || N == 7 || N == 8)
+		cout << "Summer";
+	if (N == 9 || N == 10 || N == 11)
+		cout << "Autumn";
+	if (N >= 13 && N<=100)
+		cout << "Error";
+
+}
